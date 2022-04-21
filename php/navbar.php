c2695f6985cb1a4d27dcdd3dfc1458ee8eee170a
benjaminrhodes <89805875+benjaminrhodes@users.noreply.github.com>
2022-04-21 00:14:59 -0500

    Add files via upload

---
+++ php/navbar.php
@@ -0,0 +1,25 @@
+<body>
+<!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="index.html" title="Logo">
+                    <img src="../photos/Mr. jim's logo.svg" alt="Restaurant Logo" height="100px" width="100px
+                     class="img-responsive">
+                </a>
+            </div>
+
+            <div class="topnav" id="myTopnav">
+                <a href="index.php" >Home</a>
+                <a href="products.html">Products</a>
+                <a href="categories.html">Categories</a>
+                <a href="about-us.php">About Us</a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                  <i class="fa fa-bars"></i>
+                </a>
+              </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Navbar Section Ends Here -->
